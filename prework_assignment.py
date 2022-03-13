161da1f2e30985b8248fca96b84e685c703df610
Jose Anthony Llanes <janthony022@gmail.com>
2022-03-13 08:39:16 -0700

    updated

--- prework_assignment.py
+++ prework_assignment.py
@@ -43,4 +43,13 @@ is_leap_year(2022)
 #Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
 
 def is_consecutive(a_list):
-    
+    return sorted(a_list) == list(range(min(a_list), max(a_list) + 1))
+
+a_list = [2,3,5,6,7,8,9]
+print(is_consecutive(a_list))
+
+def is_consecutive(a_list):
+    return sorted(a_list) == list(range(min(a_list), max(a_list) + 1))
+
+a_list = [1,2,3,4,5,6,7]
+print(is_consecutive(a_list))
